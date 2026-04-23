acb1122b70d5c349c1fe3ea295aa69bfed37aa71
lenovisky007 <gabriellopessc00@gmail.com>
2026-04-23 18:32:48 -0300

    fix data_loader variables

---
+++ app/data_loader.py
@@ -0,0 +1,34 @@
+KNOWN_LOW_RISK_BENEFICIARIES = [
+    "sabesp",
+    "enel",
+    "claro",
+    "vivo",
+    "tim",
+    "prefeitura",
+    "receita federal",
+    "detran",
+]
+
+CONCESSIONARIA_KEYWORDS = [
+    "energia",
+    "agua",
+    "saneamento",
+    "telefonia",
+    "internet",
+    "gas",
+]
+
+GOV_BENEFICIARY_KEYWORDS = [
+    "prefeitura",
+    "governo",
+    "receita",
+    "federal",
+    "estado",
+    "municipal",
+]
+
+KNOWN_PUBLIC_OR_SERVICE_CODES = [
+    "0001",
+    "0002",
+    "0003",
+]
