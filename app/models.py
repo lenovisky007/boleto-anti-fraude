b7950e25f65637ca728a3bff86567e944c31edf0
lenovisky007 <gabriellopessc00@gmail.com>
2026-04-23 11:07:57 -0300

    fix import database

--- app/models.py
+++ app/models.py
@@ -2,7 +2,7 @@ from sqlalchemy import Column, Integer, String, DateTime, ForeignKey, Boolean, T
 from sqlalchemy.orm import relationship
 from datetime import datetime
 
-from database import Base
+from app.database import Base
 
 
 # ============================================================
