cbdf0a091acaead9b472b97c85e22aadf9f3ac2c
lenovisky007 <gabriellopessc00@gmail.com>
2026-04-23 11:29:09 -0300

    fix: add psycopg2 driver

--- app/database.py
+++ app/database.py
@@ -1,15 +1,13 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
+if not DATABASE_URL:
+    raise Exception("DATABASE_URL não configurada no Railway")
+
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
+
 engine = create_engine(DATABASE_URL)
-
-SessionLocal = sessionmaker(
-    autocommit=False,
-    autoflush=False,
-    bind=engine
-)
-
+SessionLocal = sessionmaker(bind=engine)
 Base = declarative_base()
